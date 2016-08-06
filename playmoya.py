2dfa0884e1c3804e10513274f5d11d011addac06
Steven <kabuto93@gmail.com>
2016-08-06 12:58:30 -0400

    Initial version

---
+++ playmoya.py
@@ -0,0 +1,22 @@
+import urllib
+import re
+import json
+
+
+def searchfunction(searchterm):
+    try:
+        baseurl = "https://plamoya.com/index.php?main_page=advanced_search_result&search_in_description=0&"
+        url = baseurl + urllib.urlencode({"keyword":searchterm})
+        info = urllib.urlopen(url).read()
+        figures = re.findall('"centerBoxContentsProducts(.+?<div>.+?)</div>', info, flags=re.DOTALL)
+        stockurl = re.findall('href="(.+?)">', figures[0])[0]
+        stockinfo = urllib.urlopen(stockurl).read()
+        stock = re.findall('price.</div>.+?<div class=.+?>(.+?)<', stockinfo, flags=re.DOTALL)
+        figure = {"title": re.findall('alt="(.+?)" t', figures[0])[0],
+                  "price": re.findall('\$(.+)', figures[0])[0].replace(",", ""),
+                  "stock": stock[0]}
+        exchangerate = json.loads(urllib.urlopen("http://api.fixer.io/latest?base=USD").read())
+        figure["price"] = str(int(float(figure["price"]) * exchangerate["rates"]["JPY"]))
+        return figure
+    except:
+        return {"title": "Figure not Found"}
